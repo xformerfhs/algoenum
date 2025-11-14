6b1ede16ea50234d89eeb4ffa415b89fe215d274
Frank Schwab <3625827+xformerfhs@users.noreply.github.com>
2025-11-14 17:31:52 +0100

    fix: Removed misplaced comment.

--- bcryptenum/BcryptEnum.c
+++ bcryptenum/BcryptEnum.c
@@ -18,8 +18,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// Purpose: Main program for crypto benchmark.
-// 
 // Author: Frank Schwab
 //
 // Version: 2.0.0
